686f21cab65db21500ac7f414d10b2c325898ac4
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-03-08 22:52:46 +0500

    представления юзера

--- good_habits/users/views.py
+++ good_habits/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from good_habits.users.serliasers import UserSerializer
+from models import User
+
 
 # Create your views here.
+class UserViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
