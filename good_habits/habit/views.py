2773021717ad697277f285e28ca150469fc4ce55
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-03-13 02:37:10 +0500

    пагинатор

--- good_habits/habit/views.py
+++ good_habits/habit/views.py
@@ -3,11 +3,12 @@ from rest_framework import serializers, generics
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Habit
+from .paginators import Paginatiom
 from .serializers import HabitSerializer
 
 
 # Create your views here.
-#---------------------EasyHabit
+# ---------------------EasyHabit
 class HabitCreateAPIView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = HabitSerializer
@@ -24,15 +25,15 @@ class HabitUpdateAPIView(generics.UpdateAPIView):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
+
 class HabitListAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
+    pagination_class = Paginatiom
 
 
 class HabitDestroyAPIView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
-
-
